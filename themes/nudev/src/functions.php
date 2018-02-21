5b45b28f729416e10ab1695bcbb79f4a383b1c38
Sean Tierney <s.tierney@northeastern.edu>
2018-02-21 13:56:28 -0500

    added new functionality to provide srcset imagery from acf

--- themes/nudev/src/functions.php
+++ themes/nudev/src/functions.php
@@ -18,6 +18,40 @@ Load External Files
 
 
 
+
+
+/**
+ * Responsive Image Helper Function
+ *
+ * @param string $image_id the id of the image (from ACF or similar)
+ * @param string $image_size the size of the thumbnail image or custom image size
+ * @param string $max_width the max width this image will be shown to build the sizes attribute
+ */
+
+function ar_responsive_image($image_id,$image_size,$max_width){
+
+	// check the image ID is not blank
+	if($image_id != '') {
+
+		// set the default src image size
+		$image_src = wp_get_attachment_image_url( $image_id, $image_size );
+
+		// set the srcset with various image sizes
+		$image_srcset = wp_get_attachment_image_srcset( $image_id, $image_size );
+
+		// generate the markup for the responsive image
+		return 'src="'.$image_src.'" srcset="'.$image_srcset.'" sizes="(max-width: '.$max_width.') 100vw, '.$max_width.'"';
+
+	}
+}
+
+
+
+
+
+
+
+
 // Load any external files you have here
 
 // set up specific theme support if required
@@ -70,6 +104,20 @@ require_once(get_template_directory() . "/functions/admin-supernav.php");
 
 // set up specific admin tools for page prefooter content
 require_once(get_template_directory() . "/functions/admin-prefooter.php");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
