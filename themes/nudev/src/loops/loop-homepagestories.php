b57e9f93d3dde1ad385fa08fa388e15ec7830654
Sean Tierney <s.tierney@northeastern.edu>
2018-03-02 12:07:32 -0500

    cleanup

--- themes/nudev/src/loops/loop-homepagestories.php
+++ themes/nudev/src/loops/loop-homepagestories.php
@@ -20,9 +20,9 @@
 
 	$return = '<section class="panel-'.$jj.'">';
 
-	$guideSingle = '<article id="article-%s" style="background-image: url(%s);" ><a href="%s" title="Click here now to learn more" %s><p>%s -></p></a><h2>%s</h2>%s</article>';
+	$guideSingle = '<article id="article-%s" style="background-image: url(%s);" ><a href="%s" title="Click here now to learn more %s" %s><p>%s -></p></a><h2>%s</h2>%s</article>';
 
-	$guideRotate = '<article id="article-%s" data-rotatorid="%s" data-cslide="1" data-slidemax="%s" class="nu__block-rotator"><a href="%s" style="background-image: url(%s);" target="%s" title="Click here now to learn more"><h2>%s</h2></a><div class="slider_prev rotate" title="Click here to view the previous slide"><svg viewbox="0 0 100 100"><path class="arrow" d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z" transform=" translate(15,0)" /></svg></div><div class="slider_next rotate" title="Click here to view the next slide"><svg viewbox="0 0 100 100"><path class="arrow" d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z" transform="translate(85,100) rotate(180) " /></svg></div>%s</article>';
+	$guideRotate = '<article id="article-%s" data-rotatorid="%s" data-cslide="1" data-slidemax="%s" class="nu__block-rotator"><a href="%s" style="background-image: url(%s);" target="%s" title="Click here now to learn more %s"><h2>%s</h2></a><div class="slider_prev rotate" title="Click here to view the previous slide"><svg viewbox="0 0 100 100"><path class="arrow" d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z" transform=" translate(15,0)" /></svg></div><div class="slider_next rotate" title="Click here to view the next slide"><svg viewbox="0 0 100 100"><path class="arrow" d="M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z" transform="translate(85,100) rotate(180) " /></svg></div>%s</article>';
 
 
 	foreach($stories as $s){
@@ -33,6 +33,7 @@
 				$guideSingle
 				,$i
 				,$s['block_slide'][0]['block_slide_image']['url']
+				,($s['block_slide'][0]['external_link'] == '1'?' [will open in new window]':'')
 				,$s['block_slide'][0]['block_slide_link']
 				,($s['block_slide'][0]['external_link'] == '1'?' target="_blank"':'')
 				,$s['block_slide'][0]['block_slide_description']
@@ -50,6 +51,7 @@
 				,$s['block_slide'][0]['block_slide_link']
 				,$s['block_slide'][0]['block_slide_image']['url']
 				,($s['block_slide'][0]['external_link'] == '1'?'_blank':'')
+				,($s['block_slide'][0]['external_link'] == '1'?' [will open in new window]':'')
 				,$s['block_slide'][0]['block_slide_title']
 				,(isset($s['block_overlay_logo']) && $s['block_overlay_logo'] != ''?'<div class="nu__overlay-logo"><img src="'.$s['block_overlay_logo']['url'].'" alt="overlay logo" /></div>':'')
 			);
