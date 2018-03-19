3e25a4c417ced1ca662cdacff9c5b5509da188d1
Sean Tierney <s.tierney@northeastern.edu>
2018-03-19 08:42:42 -0400

    trying to get the default template to redirect

--- themes/nudev/src/page.php
+++ themes/nudev/src/page.php
@@ -2,6 +2,7 @@
 	// this is a redirect to the homepage to prevent users from reaching null pages
 	// header('location:'.site_url());
 	// exit();
-	wp_redirect(site_url());
+	// wp_redirect(site_url());
+	wp_redirect('//newnu.nudev.net');
 	exit;
 ?>
