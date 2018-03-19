cb0faec9478e12f7cdcde168467235a6260f414f
Sean Tierney <s.tierney@northeastern.edu>
2018-03-19 08:38:08 -0400

    trying to get the null page default template redirect to work

--- themes/nudev/src/page.php
+++ themes/nudev/src/page.php
@@ -1,6 +1,7 @@
 <?
-	header('location:'.site_url());
-	exit();
-
-	
+	// this is a redirect to the homepage to prevent users from reaching null pages
+	// header('location:'.site_url());
+	// exit();
+	wp_redirect(site_url());
+	exit;
 ?>
