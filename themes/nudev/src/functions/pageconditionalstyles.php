00e8f39883c5cfb4d9590c0d727ad73f129148a5
Sean Tierney <s.tierney@northeastern.edu>
2018-03-26 17:38:43 -0400

    fixed an issue with the debounce on the homepage as it was bleeding into other pages

--- themes/nudev/src/functions/pageconditionalstyles.php
+++ themes/nudev/src/functions/pageconditionalstyles.php
@@ -26,7 +26,8 @@ function nudev_conditional_styles(){
 
 
   // load homepage styles
-  if(is_page('')){
+  // if(is_page('')){
+  if(is_page_template('templates/template-homepage.php')){
     wp_register_style('homepagecss', get_template_directory_uri() . '/css/homepage.css', array(), '1.0');
     wp_enqueue_style('homepagecss');
   }
