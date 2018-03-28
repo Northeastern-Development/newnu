8f533d32205f436198a5de4edda2077bdcbe1962
Sean Tierney <s.tierney@northeastern.edu>
2018-03-28 10:38:57 -0400

    new template for experience page

---
+++ themes/nudev/src/templates/template-experience.php
@@ -0,0 +1,62 @@
+<?php
+
+	/* Template Name: Experience */
+
+	get_header();
+
+?>
+
+	<main id="nu__static" role="main" aria-label="content">
+
+		<section class="hero">
+			<div>
+				<h2>Experience: The push that drives us</h2>
+				<h3>Northeastern University is powered by a simple truth: Experience deepens learning and discovery like nothing else can.</h3>
+			</div>
+		</section>
+
+		<section class="leftright">
+			<a href="http://cnn.com" title="Experience amplifies undergraduate learning" target="_blank">
+				<div class="content">
+					<div>
+						<h3>Experience amplifies undergraduate learning.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+				</div>
+				<div class="image">
+					<div style="background:url(http://fpoimagery.com/?t=px&w=650&h=300);"></div>
+				</div>
+			</a>
+		</section>
+
+		<section class="leftright">
+			<a href="http://cnn.com" title="Experience ignites discovery" target="_blank">
+				<div class="image">
+					<div style="background:url(http://fpoimagery.com/?t=px&w=650&h=300);"></div>
+				</div>
+				<div class="content">
+					<div>
+						<h3>Experience ignites discovery.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div>
+				</div>
+			</a>
+		</section>
+
+		<section class="leftright">
+			<a href="http://cnn.com" title="Experience makes learning lifelong" target="_blank">
+				<div class="content">
+					<div>
+						<h3>Experience makes learning lifelong.</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+					</div>
+				</div>
+				<div class="image">
+					<div style="background:url(http://fpoimagery.com/?t=px&w=650&h=300);"></div>
+				</div>
+			</a>
+		</section>
+
+	</main>
+
+<?php get_footer(); ?>
