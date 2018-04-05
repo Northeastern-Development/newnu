1f33c047c31df4ac4cbde452c5751a50d3031e0c
Sean Tierney <s.tierney@northeastern.edu>
2018-04-05 13:19:40 -0400

    tweaks to homepage layout and fixes for the new overlay N to not blow out the page and force a scroll

--- themes/nudev/src/loops/loop-homepagestories.php
+++ themes/nudev/src/loops/loop-homepagestories.php
@@ -22,9 +22,9 @@
 
 	$return = '<section class="panel-'.$jj.'">';
 
-	$guideSingle = '<article id="article-%s" style="%s %s"><a href="%s" title="Click here now to learn more %s" %s>%s</a><div class="nu__panel-content"><p>%s&nbsp;</p><h2><span>%s</span></h2></div>%s<div class="gradient"></div>%s</article>';
+	$guideSingle = '<article id="article-%s" style="%s %s"><a href="%s" title="Click here now to learn more %s" %s>%s</a><div class="nu__panel-content"><div><p>%s&nbsp;</p><h2><span>%s</span></h2></div></div>%s<div class="gradient"></div>%s</article>';
 
-	$guideRotate = '<article id="article-%s" data-rotatorid="%s" data-cslide="1" data-slidemax="%s" class="nu__block-rotator" style="%s %s"><a href="%s" target="%s" title="Click here now to learn more %s">%s</a><div class="nu__panel-content"><p>%s&nbsp;</p><h2><span>%s</span></h2></div><div class="nu__slide-controls"><div><div class="slider_prev rotate" title="Click here to view the previous slide">&#xE5C4;</div><div class="slider_next rotate" title="Click here to view the next slide">&#xE5C8;</div></div></div><div class="nu__overlay-logo">%s</div><div class="gradient"></div></article>';
+	$guideRotate = '<article id="article-%s" data-rotatorid="%s" data-cslide="1" data-slidemax="%s" class="nu__block-rotator" style="%s %s"><a href="%s" target="%s" title="Click here now to learn more %s">%s</a><div class="nu__panel-content"><div><p>%s&nbsp;</p><h2><span>%s</span></h2></div></div><div class="nu__slide-controls"><div><div class="slider_prev rotate" title="Click here to view the previous slide">&#xE5C4;</div><div class="slider_next rotate" title="Click here to view the next slide">&#xE5C8;</div></div></div><div class="nu__overlay-logo">%s</div><div class="gradient"></div></article>';
 
 	$iPath = responsive_background_images();	// call the function to figure out the best image size to use
 
@@ -49,7 +49,7 @@
 				,$s['block_slide'][0]['block_slide_description']
 				,$s['block_slide'][0]['block_slide_title']
 				,(isset($s['block_overlay_logo']) && $s['block_overlay_logo'] != ''?'<div class="nu__overlay-logo"><img src="'.$s['block_overlay_logo']['url'].'" alt="overlay logo for '.$s['block_slide'][0]['block_slide_title'].'" /></div>':'')
-				,($i == 1 || $i == 6 || $i == 11?'<div class="nu__article-alphatile"><svg id="Layer_1" data-name="alphatile n overlay" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 360.6 336.43"><title>alphatile n overlay</title><path class="cls-1" d="M-72.09,0H17.75L220.14,248.24V57.32q0-30.52-6.83-38.09Q204.28,9,184.74,9H173.27V0H288.5V9H276.79q-21,0-29.79,12.69-5.37,7.81-5.37,35.65V336.37h-8.79L14.58,69.77V273.63q0,30.51,6.59,38.08Q30.44,322,49.73,322H61.45v9H-53.78v-9h11.47q21.24,0,30-12.7,5.37-7.81,5.37-35.64V43.4q-14.4-16.84-21.85-22.21t-21.85-10Q-57.69,9-72.09,9Z" transform="translate(72.09 0.05)"/></svg></div>':'')
+				,($i == 1 || $i == 6 || $i == 11?'<div class="nu__article-alphatile"><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 445.84 269.62"><path class="cls-1" d="M348.41,269.62C266.3,189.69,96.3,24.18,71.74,0H0V266.86c0,.94,0,1.86,0,2.76H13.48L13.07,53.76l224,215.86Z"/><path class="cls-1" d="M328.44,0l.05,2.87c36.11-.35,55.65,17.67,55.65,65V269.62h12.55c0-82,0-175.86,0-201.73-.07-48.52,22.55-64.68,49.18-64.68V0Z"/></svg></div>':'')
 			);
 
 		}else{	// this will handle rotator type articles
