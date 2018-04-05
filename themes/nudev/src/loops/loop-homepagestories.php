111fc68d6148ef1f964f28411fb152670e7c1609
Sean Tierney <s.tierney@northeastern.edu>
2018-04-05 19:37:50 -0400

    fix to prevent css bleed from svg items

--- themes/nudev/src/loops/loop-homepagestories.php
+++ themes/nudev/src/loops/loop-homepagestories.php
@@ -49,7 +49,7 @@
 				,$s['block_slide'][0]['block_slide_description']
 				,$s['block_slide'][0]['block_slide_title']
 				,(isset($s['block_overlay_logo']) && $s['block_overlay_logo'] != ''?'<div class="nu__overlay-logo"><img src="'.$s['block_overlay_logo']['url'].'" alt="overlay logo for '.$s['block_slide'][0]['block_slide_title'].'" /></div>':'')
-				,($i == 1 || $i == 6 || $i == 11?'<div class="nu__article-alphatile"><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 445.84 269.62"><path class="cls-1" d="M348.41,269.62C266.3,189.69,96.3,24.18,71.74,0H0V266.86c0,.94,0,1.86,0,2.76H13.48L13.07,53.76l224,215.86Z"/><path class="cls-1" d="M328.44,0l.05,2.87c36.11-.35,55.65,17.67,55.65,65V269.62h12.55c0-82,0-175.86,0-201.73-.07-48.52,22.55-64.68,49.18-64.68V0Z"/></svg></div>':'')
+				,($i == 1 || $i == 6 || $i == 11?'<div class="nu__article-alphatile"><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 445.84 269.62"><path class="alphatilesvg-1" d="M348.41,269.62C266.3,189.69,96.3,24.18,71.74,0H0V266.86c0,.94,0,1.86,0,2.76H13.48L13.07,53.76l224,215.86Z"/><path class="alphatilesvg-2" d="M328.44,0l.05,2.87c36.11-.35,55.65,17.67,55.65,65V269.62h12.55c0-82,0-175.86,0-201.73-.07-48.52,22.55-64.68,49.18-64.68V0Z"/></svg></div>':'')
 			);
 
 		}else{	// this will handle rotator type articles
