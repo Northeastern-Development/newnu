2b16586cf8b513cf38aa4cf943ad1820607a1f7b
Sean Tierney <s.tierney@northeastern.edu>
2018-09-17 10:56:46 -0400

    added ability to set multiple images for each of the 4 categories on the homepage, as well as control the 3 links in each category via the CMS

--- themes/nudev/src/loops/loop-homepagecategories.php
+++ themes/nudev/src/loops/loop-homepagecategories.php
@@ -14,49 +14,33 @@
 
 	$return = '';
 
-	$categoryItems = array(
-		"undergraduate" => array(
-			array('Academic Programs','https://undergraduate.northeastern.edu/academic-programs/')
-			,array('Admissions','https://www.northeastern.edu/admissions/')
-			,array('Visit Campus','https://www.northeastern.edu/admissions/connect/visit/')
-		)
-		,"graduate" => array(
-			array('Graduate Programs','https://www.northeastern.edu/graduate/programs/#/')
-			,array('Admissions','https://www.northeastern.edu/graduate/programs/#/')
-			,array('PhD Education','https://phd.northeastern.edu/')
-		)
-		,"research" => array(
-			array('Health','http://www.northeastern.edu/research/about/research-areas/health/')
-			,array('Security','http://www.northeastern.edu/research/about/research-areas/security/')
-			,array('Sustainability','http://www.northeastern.edu/research/about/research-areas/sustainability/')
-		)
-		,"lifelong learning" => array(
-			array('Professional Programs','https://www.northeastern.edu/learnforlife/programs/')
-			,array('Campus Network','https://www.northeastern.edu/learnforlife/campuses/')
-			,array('Admissions','https://www.northeastern.edu/graduate/admissions-information/how-to-apply/the-process/')
-		)
-	);
-
 	$guide = '<div><div><a href="%s" title="%s%s"%s><div class="bgimage" style="%s"></div><h2>%s</h2></a><ul>%s</ul></div></div>';
-
-	$iPath = responsive_background_images();	// call the function to figure out the best image size to use
 
 	for($i=2;$i<6;$i++){
 
 		// we need to see if there is more than 1 block with an image so that we can potentially pick a random one each time page loads
-		$thisImage = $stories[$i]['block_slide'][rand(0,(count($stories[$i]['block_slide']) - 1))]['block_slide_image']['url'];
+		$images = array();
+
+		foreach($stories[$i]['block_slide'] as $bS){
+			if($bS['block_slide_image'] != ""){
+				$images[] = $bS['block_slide_image']['url'];
+			}
+		}
+
+		$thisImage = $images[rand(0,(count($images) - 1))];
 
 		// this will build out the category options
 		$options = '';
-		foreach($categoryItems[strtolower($stories[$i]['block_name'])] as $cI){
-			$options .= '<li><a href="'.$cI[1].'" title="'.$cI[0].'" target="_blank">'.$cI[0].'</a></li>';
+
+		for($ii=1;$ii<count($stories[$i]['block_slide']);$ii++){
+			$options .= '<li><a href="'.$stories[$i]['block_slide'][$ii]['block_slide_link'].'" title="'.$stories[$i]['block_slide'][$ii]['block_slide_title'].($stories[$i]['block_slide'][$ii]['external_link'] == 1?' [will open in new tab or window]':'').'"'.($stories[$i]['block_slide'][$ii]['external_link'] == 1?' target="_blank"':'').'>'.$stories[$i]['block_slide'][$ii]['block_slide_title'].'</a></li>';
 		}
 
 		$return .= sprintf(
 			$guide
 			,$stories[$i]['block_slide'][0]['block_slide_link']
 			,$stories[$i]['block_name']
-			,($stories[$i]['block_slide'][0]['external_link'] == '1'?' [will open in new window]':'')
+			,($stories[$i]['block_slide'][0]['external_link'] == '1'?' [will open in new tab or window]':'')
 			,($stories[$i]['block_slide'][0]['external_link'] == '1'?' target="_blank"':'')
 			,'background-image: url('.$thisImage.');'
 			,$stories[$i]['block_name']
