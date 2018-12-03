88f33247d9310f231b0adb528547163645150040
Sean Tierney <s.tierney@northeastern.edu>
2018-12-03 15:33:20 -0500

    tweaks to the layout of the misc. items and overall first row on the homepage

--- themes/nudev/src/loops/loop-homepagemisc.php
+++ themes/nudev/src/loops/loop-homepagemisc.php
@@ -12,7 +12,9 @@
 
 	$story = $fields['top_story_blocks'][1];
 
-	$guide = '<article id="article-%s"><a %s href="%s" title="Click here now to learn more about the northeastern experience%s" aria-label="Click here now to learn more about the northeastern experience%s" %s>%s</a><div class="bgimage" style="%s %s"></div><div class="nu__panel-content"><div><h2><span>%s</span></h2></div></div>%s<div class="gradient"></div>%s</article>';
+	// $guide = '<article id="article-%s"><div class="bgimage" style="%s %s"></div><div class="nu__panel-content"><div><h2><span>%s</span></h2><a href="" title="" aria-label="">Learn More</a></div></div>%s<div class="gradient"></div>%s</article>';
+
+	$guide = '<article id="article-%s"><div class="bgimage" style="%s %s"></div><div class="nu__panel-content"><div><h2><span>%s</span></h2><a href="%s" title="Learn more - %s%s" aria-label="Learn more - %s%s"%s>Learn More</a></div></div>%s<div class="gradient"></div></article>';
 
 	// $iPath = responsive_background_images('large');	// call the function to figure out the best image size to use
 
@@ -24,17 +26,23 @@
 	echo sprintf(
 		$guide
 		,'2'
-		,($i > 5?'tabindex="-1"':'')
-		,$story['block_slide'][0]['block_slide_link']
-		,($story['block_slide'][0]['external_link'] == '1'?' [will open in new window]':'')
-		,($story['block_slide'][0]['external_link'] == '1'?' target="_blank"':'')
-		,($story['block_slide'][0]['external_link'] == '1'?' target="_blank"':'')
-		,$story['block_slide'][0]['block_slide_title']
+		// ,($i > 5?'tabindex="-1"':'')
+		// ,$story['block_slide'][0]['block_slide_link']
+		// ,($story['block_slide'][0]['external_link'] == '1'?' [will open in new window]':'')
+		// ,($story['block_slide'][0]['external_link'] == '1'?' target="_blank"':'')
+		// ,($story['block_slide'][0]['external_link'] == '1'?' target="_blank"':'')
+		// ,$story['block_slide'][0]['block_slide_title']
 		,'background-image: url('.$thisImage['block_slide_image']['url'].');'
 		,'background-color: '.$story['block_slide'][0]['block_slide_background'].'; '
 		,$story['block_slide'][0]['block_slide_title']
+		,$story['block_slide'][0]['block_slide_link']
+		,$story['block_slide'][0]['block_slide_title']
+		,($story['block_slide'][0]['external_link'] == '1'?' [will open in new window]':'')
+		,$story['block_slide'][0]['block_slide_title']
+		,($story['block_slide'][0]['external_link'] == '1'?' [will open in new window]':'')
+		,($story['block_slide'][0]['external_link'] == '1'?' target="_blank"':'')
 		,(isset($story['block_overlay_logo']) && $story['block_overlay_logo'] != ''?'<div class="nu__overlay-logo"><img src="'.$story['block_overlay_logo']['url'].'" alt="overlay logo for '.$story['block_slide'][0]['block_slide_title'].'" /></div>':'')
-		,($i == 1 || $i == 6 || $i == 11?'<div class="nu__article-alphatile"><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 445.84 269.62"><path class="alphatilesvg-1" d="M348.41,269.62C266.3,189.69,96.3,24.18,71.74,0H0V266.86c0,.94,0,1.86,0,2.76H13.48L13.07,53.76l224,215.86Z"/><path class="alphatilesvg-2" d="M328.44,0l.05,2.87c36.11-.35,55.65,17.67,55.65,65V269.62h12.55c0-82,0-175.86,0-201.73-.07-48.52,22.55-64.68,49.18-64.68V0Z"/></svg></div>':'')
+		// ,($i == 1 || $i == 6 || $i == 11?'<div class="nu__article-alphatile"><svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 445.84 269.62"><path class="alphatilesvg-1" d="M348.41,269.62C266.3,189.69,96.3,24.18,71.74,0H0V266.86c0,.94,0,1.86,0,2.76H13.48L13.07,53.76l224,215.86Z"/><path class="alphatilesvg-2" d="M328.44,0l.05,2.87c36.11-.35,55.65,17.67,55.65,65V269.62h12.55c0-82,0-175.86,0-201.73-.07-48.52,22.55-64.68,49.18-64.68V0Z"/></svg></div>':'')
 	);
 
 ?>
