c79c94888f9331b840ad0dca65ace6425c51ecb0
Sean Tierney <s.tierney@northeastern.edu>
2019-03-28 12:50:54 -0400

    some final ADA changes and clean up

--- themes/nudev/src/footer.php
+++ themes/nudev/src/footer.php
@@ -1,12 +1,5 @@
 		</div>
 
-		<div class="cookiewarning">
-			<div class="js__cookie-accept" title="Click here to accept and continue">&#xE14C;</div>
-			<h3>Cookies on Northeastern sites</h3>
-			<p>This website uses cookies and similar technologies to understand your use of our website and give you a better experience. By continuing to use the site or closing this banner without changing your cookie settings, you agree to our use of cookies and other technologies. To find out more about our use of cookies and how to change your settings, please go to our <a href="/privacy-information" title="Privacy information">Privacy Statement</a>.</p>
-			<button class="js__cookie-accept" title="Click here to accept and continue">Accept and Continue</button>
-		</div>
-
 		<?php
 
 			global $forcePageID;	// check to see if we are manually overriding the natural WP page ID
@@ -17,6 +10,13 @@
 		?>
 
 		<footer id="nu__global-footer" class="<?=($prefooter?'addprefooter ':'')?><?=(trim($_SERVER['REQUEST_URI']) === '/'?'collapse absolute':'')?>">
+
+			<div class="cookiewarning">
+				<div class="js__cookie-accept" title="Click here to accept and continue">&#xE14C;</div>
+				<h3>Cookies on Northeastern sites</h3>
+				<p>This website uses cookies and similar technologies to understand your use of our website and give you a better experience. By continuing to use the site or closing this banner without changing your cookie settings, you agree to our use of cookies and other technologies. To find out more about our use of cookies and how to change your settings, please go to our <a href="/privacy-information" title="Privacy information">Privacy Statement</a>.</p>
+				<button class="js__cookie-accept" title="Click here to accept and continue">Accept and Continue</button>
+			</div>
 
 			<?php
 
