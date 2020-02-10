708d0ae28d01475e899d3d10c3211627aec170ff
Sean Tierney <s.tierney@northeastern.edu>
2020-02-10 16:44:18 -0500

    tweaks to move the corona announcement down the usual experience block next to news...added new CMS functionality to allow for custom class to be added and extra data collected

--- themes/nudev/src/loops/loop-homepagemisc.php
+++ themes/nudev/src/loops/loop-homepagemisc.php
@@ -12,16 +12,17 @@
 
 	$story = $fields['top_story_blocks'][1];
 
-	$story['block_slide'][0]['block_slide_cta'] = 'Take The Challenge';	// this is for giving day!!! comment out when no longer needed!!!
+	// $story['block_slide'][0]['block_slide_cta'] = 'Learn More';	// this is for giving day!!! comment out when no longer needed!!!
+	$cta = (!empty($story['block_slide'][0]['slide_button_copy'])?$story['block_slide'][0]['slide_button_copy']:'Learn More');
 
-	$guide = '<article id="article-%s" class="flexspace"><div class="bgimage" style="%s %s"></div><div class="nu__panel-content"><div><h2><span>%s</span></h2><a class="learnmore" href="%s" title="%s%s" aria-label="%s%s"%s>%s</a></div></div>%s<div class="gradient"></div></article>';
+	$guide = '<article id="article-%s"'.($story['block_slide'][0]['flexspace'] == 1?' class="flexspace"':'').'><div class="bgimage" style="%s %s"></div><div class="nu__panel-content"><div><h2><span>%s</span></h2><a class="learnmore" href="%s" title="%s%s" aria-label="%s%s"%s>%s</a></div></div>%s<div class="gradient"></div></article>';
 
-	$thisImage = $story['block_slide'][0];
+	// $thisImage = $story['block_slide'][0];
 
 	echo sprintf(
 		$guide
 		,'2'
-		,'background-image: url('.$thisImage['block_slide_image']['url'].');'
+		,'background-image: url('.$story['block_slide'][0]['block_slide_image']['url'].');'
 		,'background-color: '.$story['block_slide'][0]['block_slide_background'].'; '
 		,$story['block_slide'][0]['block_slide_title']
 		,$story['block_slide'][0]['block_slide_link']
@@ -30,7 +31,8 @@
 		,$story['block_slide'][0]['block_slide_title']
 		,($story['block_slide'][0]['external_link'] == '1'?' [will open in new window]':'')
 		,($story['block_slide'][0]['external_link'] == '1'?' target="_blank" rel="noopener"':'')
-		,(!empty($story['block_slide'][0]['block_slide_cta'])?$story['block_slide'][0]['block_slide_cta']:'Learn More')
+		// ,(!empty($story['block_slide'][0]['block_slide_cta'])?$story['block_slide'][0]['block_slide_cta']:'Learn More')
+		,$cta
 		,(isset($story['block_overlay_logo']) && $story['block_overlay_logo'] != ''?'<div class="nu__overlay-logo"><img src="'.$story['block_overlay_logo']['url'].'" alt="overlay logo for '.$story['block_slide'][0]['block_slide_title'].'" /></div>':'')
 	);
 
